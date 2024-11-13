64d48804d1a9f5885a92a5d19724ee96fcab6955
Foks <saaamushkalov@gmail.com>
2024-11-13 10:55:16 +0500

    Config .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,22 +3,25 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './users/entities/user.entity';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     UsersModule,
     ConfigModule.forRoot(),
-    TypeOrmModule.forRoot({
-      type: 'mariadb',
-      host: '81.31.247.100',
-      port: 3306,
-      username: 'uazPBI',
-      password: 'AgXQistIcKKjnXAg',
-      database: 'rACnoyZh',
-      entities: [User],
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mariadb',
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_BASE'),
+        synchronize: true,
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      }),
+      inject: [ConfigService],
     }),
   ],
   controllers: [AppController],
